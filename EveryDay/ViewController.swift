17630c43a64e74921a307575132101b20451f996
Ruslan Sadritdinov <rus9606@icloud.com>
2022-05-19 21:15:06 +0300

    Initial Commit

---
+++ EveryDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EveryDay
+//
+//  Created by Ruslan Sadritdinov on 19.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
